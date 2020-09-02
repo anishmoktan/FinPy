a5a9f803f43dcc42327c090cc6bd70c76b26c437
anishmoktan <anish.tmngo@gmail.com>
2020-09-02 07:46:30 -0400

    start

---
+++ main.py
@@ -0,0 +1,5 @@
+#The goal of this project is to apply Time Value of Money theories to create a financial calulator 
+#I will be using formulas from Financial Mathematics Actuarial Science exam as reference 
+#First I will use the terminal for calculations then create a front end to create a working website 
+
+principal  = input("Please enter the initial investment ")
