ff91240dd16f73b6df23f1572cdd69acffc3fcd6
anishmoktan <anish.tmngo@gmail.com>
2020-09-11 08:22:05 -0400

    okay

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 #I will be using formulas from Financial Mathematics Actuarial Science exam as reference 
 #First I will use the terminal for calculations then create a front end to create a working website 
 
+#Import classes from other files 
+#Use Flask to connect to front end.
+
+
 principal  = int(input("Please enter the initial investment: "))
 interest = int(input("Please enter the interest rate: "))
 term = int(input("Please enter the length of your loan: "))
