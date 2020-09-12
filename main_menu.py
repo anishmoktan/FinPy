0a7cc346694df057dd4135ba321a671cbd7aaee3
anishmoktan <anish.tmngo@gmail.com>
2020-09-12 10:46:49 -0400

    Welcome

--- main_menu.py
+++ main_menu.py
@@ -1,1 +1,8 @@
 #This is where the main menu will be
+
+welcome = """
+
+Hi welcome to loan calculator, please enter the inital loan value, the term, and the interest rate to move forward with the calculations!
+
+
+"""
